a5bd0c046dd41f509167efa11416b375a255b6d5
奋斗的胖轩 <2737292143@qq.com>
2019-03-11 21:08:10 +0800

    更新注释

--- src/views/store/StoreDetail.vue
+++ src/views/store/StoreDetail.vue
@@ -4,9 +4,11 @@
     <detail-title @back="back" :showShelf="true" ref="title"></detail-title>
     <scroll class="content-wrapper" :top="42" :bottom="52" @onScroll="onScroll" ref="scroll">
       <book-info :cover="cover" :title="title" :author="author" :desc="desc"></book-info>
+      <!--图书详细信息包裹，第一个是图书版权信息-->
       <div class="book-detail-content-wrapper">
         <div class="book-detail-content-title">{{$t('detail.copyright')}}</div>
         <div class="book-detail-content-list-wrapper">
+          <!--可以考虑将每一行抽象成一个组件，传入不同的值，这样可简化代码-->
           <div class="book-detail-content-row">
             <div class="book-detail-content-label">{{$t('detail.publisher')}}</div>
             <div class="book-detail-content-text">{{publisher}}</div>
@@ -25,15 +27,21 @@
           </div>
         </div>
       </div>
+      <!--图书详细信息包裹，第二个是图书目录信息-->
       <div class="book-detail-content-wrapper">
         <div class="book-detail-content-title">{{$t('detail.navigation')}}</div>
         <div class="book-detail-content-list-wrapper">
+          <!--目录不存在时，显示加载-->
           <div class="loading-text-wrapper" v-if="!this.navigation">
             <span class="loading-text">{{$t('detail.loading')}}</span>
           </div>
+          <!--目录包裹层-->
           <div class="book-detail-content-item-wrapper">
+            <!--单击目录跳转进入阅读器界面，这是目录每行包裹层-->
             <div class="book-detail-content-item" v-for="(item, index) in flatNavigation" :key="index"
                  @click="read(item)">
+              <!--具体的目录每一行。这里也会对目录层级进行判断从而缩进，方法与前面介绍的一样
+              章节名称存在时才渲染此项目录。-->
               <div class="book-detail-content-navigation-text"
                    :class="{'is-sub': item.deep > 1}"
                    :style="itemStyle(item)"
@@ -43,16 +51,19 @@
           </div>
         </div>
       </div>
+      <!--图书详细信息包裹层，第三个是试读部分-->
       <div class="book-detail-content-wrapper">
-        <div class="book-detail-content-title">{{$t('detail.trial')}}</div>
-        <div class="book-detail-content-list-wrapper">
+        <div class="book-detai-l-content-title">{{$t('detail.trial')}}</div>
+        <div class="book-detail-content-listwrapper">
           <div class="loading-text-wrapper" v-if="!this.displayed">
             <span class="loading-text">{{$t('detail.loading')}}</span>
           </div>
         </div>
+        <!--预览部分-->
         <div id="preview" v-show="this.displayed" ref="preview"></div>
       </div>
     </scroll>
+    <!--立即阅读部分-->
     <div class="bottom-wrapper">
       <div class="bottom-btn" @click.stop.prevent="readBook()">{{$t('detail.read')}}</div>
       <!--<div class="bottom-btn" @click.stop.prevent="trialListening()">{{$t('detail.listen')}}</div>-->
@@ -118,34 +129,34 @@
       author () {
         return this.metadata ? this.metadata.creator : ''
       },
-      inBookShelf () {
-        if (this.bookItem && this.bookShelf) {
-          const flatShelf = (function flatten (arr) {
-            return [].concat(...arr.map(v => v.itemList ? [v, ...flatten(v.itemList)] : v))
-          })(this.bookShelf).filter(item => item.type === 1);
-          const book = flatShelf.filter(item => item.fileName === this.bookItem.fileName);
-          return book && book.length > 0
-        } else {
-          return false
-        }
-      }
+//      inBookShelf () {
+//        if (this.bookItem && this.bookShelf) {
+//          const flatShelf = (function flatten (arr) {
+//            return [].concat(...arr.map(v => v.itemList ? [v, ...flatten(v.itemList)] : v))
+//          })(this.bookShelf).filter(item => item.type === 1);
+//          const book = flatShelf.filter(item => item.fileName === this.bookItem.fileName);
+//          return book && book.length > 0
+//        } else {
+//          return false
+//        }
+//      }
     },
     data () {
       return {
         bookItem: null,
         book: null,
-        metadata: null,
+        metadata: null, // 电子书标题作者等信息
         trialRead: null,
-        cover: null,
-        navigation: null,
-        displayed: false,
+        cover: null, // 电子书封面
+        navigation: null, // 存储电子书的目录
+        displayed: false, // 判断电子书是否渲染完毕
         audio: null,
         randomLocation: null,
         description: null, // 电子书简要介绍数据
         toastText: '',
         trialText: null,
         categoryText: null,
-        opf: null
+        opf: null // 电子书content.opf文件路径
       }
     },
     methods: {
@@ -168,11 +179,13 @@
           path: `/ebook/${this.categoryText}|${this.fileName}`
         })
       },
+      // 子级目录缩进
       itemStyle (item) {
         return {
           marginLeft: (item.deep - 1) * px2rem(20) + 'rem'
         }
       },
+      // 给目录加上层级并拍平
       doFlatNavigation (content, deep = 1) {
         const arr = [];
         content.forEach(item => {
@@ -188,22 +201,29 @@
         const opf = `${process.env.VUE_APP_EPUB_URL}/${this.bookItem.categoryText}/${this.bookItem.fileName}/OEBPS/package.opf`;
         this.parseBook(opf)
       },
-      // 根据路径将电子书解析为对象，loaded为钩子函数.参数为opf路径。在电子书阅读部分中，下载路径也可以解析为对象。opf不用下载电子书
+      // 根据路径将电子书解析为对象，loaded为钩子函数.参数为opf路径。
+      // 当传入整本电子书的路径时，epub解析的第一步也是找到content.opf文件，它包含了整本电子书的资源文件。
+      // 这样做的好处是预览时避免了下载整本电子书，也能获取相关资源
       parseBook (url) {
         this.book = new Epub(url);
+        // 获取电子书的标题作者等信息
         this.book.loaded.metadata.then(metadata => {
           this.metadata = metadata
         });
+        // 获取电子书的目录信息
         this.book.loaded.navigation.then(nav => {
           this.navigation = nav;
           if (this.navigation.toc && this.navigation.toc.length > 1) {
+            // 把第二章节(也就是去除了封面章节的第一个章节)的地址传入渲染函数，返回Promise对象方便我们操作
             const candisplay = this.display(this.navigation.toc[1].href);
             if (candisplay) {
               candisplay.then(section => {
+                // 渲染那一页后，display会返回一个该章节信息的对象。
                 if (this.$refs.scroll) {
                   this.$refs.scroll.refresh()
                 }
                 this.displayed = true;
+                // 打印section对象
                 const reg = new RegExp('<.+?>', 'g');
                 const text = section.output.replace(reg, '').replace(/\s\s/g, '');
                 this.description = text
@@ -212,6 +232,7 @@
           }
         })
       },
+      // 详情页面加载完毕后立即调用初始化函数
       init () {
         this.fileName = this.$route.query.fileName;
         this.categoryText = this.$route.query.category;
@@ -227,6 +248,7 @@
               if (rootFile.startsWith('/')) {
                 rootFile = rootFile.substring(1, rootFile.length)
               }
+              // 获取电子书的content.opf文件路径，传入解析电子书的函数内
               this.opf = `${process.env.VUE_APP_EPUB_OPF_URL}/${this.fileName}/${rootFile}`;
               this.parseBook(this.opf)
             } else {
@@ -239,6 +261,7 @@
       back () {
         this.$router.go(-1)
       },
+      // 渲染函数。判断DOM是否存在以及渲染对象是否生成，没有的话则生成渲染对象，并挂载到DOM上
       display (location) {
         if (this.$refs.preview) {
           if (!this.rendition) {
@@ -248,6 +271,7 @@
               method: 'default'
             })
           }
+          // 如果传入了位置，则渲染扉页。
           if (!location) {
             return this.rendition.display()
           } else {
